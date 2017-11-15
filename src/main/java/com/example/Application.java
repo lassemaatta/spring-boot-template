dfdaf8898558a0d3b6239e066bd6e4c370aa4676
Lasse Maatta <lasse.olavi.maatta@gmail.com>
2017-11-15 20:03:35 +0200

    Initial pom.xml and dummy Application

---
+++ src/main/java/com/example/Application.java
@@ -0,0 +1,32 @@
+package com.example;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(final String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    public CommandLineRunner commandLineRunner(final ApplicationContext ctx) {
+        return args -> {
+
+            System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            final String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (final String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+
+        };
+    }
+}
